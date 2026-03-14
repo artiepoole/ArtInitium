ecd7231ff19ba3176f2bfe64a66ef5ac2e6bd9eb
artiepoole <stuart.f.poole@gmail.com>
2026-03-14 21:07:10 +0000

    arm64: add dtb.zig to lib - template only

---
+++ src/lib/arm64/dtb/dtb.zig
@@ -0,0 +1,13 @@
+// ArtInitium - MultiArch Bootloader qemu
+//
+// SPDX-License-Identifier: MIT
+// Copyright (c) 2026 Artie Poole
+const std = @import("std");
+
+/// Reference: Device Tree Specification, version 0.4
+/// https://github.com/devicetree-org/devicetree-specification/releases/download/v0.4/devicetree-specification-v0.4.pdf
+/// Reference: Device Tree Specification, Flattened Device Tree (FDT) format
+/// https://devicetree-specification.readthedocs.io/en/stable/flattened-format.html
+pub fn foo() !void {
+
+}
