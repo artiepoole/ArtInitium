8431424a89f51b04315c6366eba7bd6c78ede7d9
artiepoole <stuart.f.poole@gmail.com>
2026-03-15 11:22:59 +0000

    arm64: dtb parser - just moving stuff

--- src/lib/arm64/dtb/fdt.zig
+++ src/lib/arm64/dtb/fdt.zig
@@ -6,30 +6,38 @@
 /// Raw FDT (Flattened Device Tree) structures and token definitions.
 /// Ref: Devicetree Specification v0.4
 /// https://devicetree-specification.readthedocs.io/en/latest/chapter5-flattened-format.html
-
 const std = @import("std");
-
-comptime { _ = std; }
 
 pub const MAGIC: u32 = 0xd00dfeed;
 
 /// FDT structure block token types.
 pub const Token = enum(u32) {
     begin_node = 0x00000001, // Start of a node; followed by null-terminated name
-    end_node   = 0x00000002, // End of a node
-    prop       = 0x00000003, // Property; followed by PropHeader then data
-    nop        = 0x00000004, // No-op, skip
-    end        = 0x00000009, // End of structure block
+    end_node = 0x00000002, // End of a node
+    prop = 0x00000003, // Property; followed by PropHeader then data
+    nop = 0x00000004, // No-op, skip
+    end = 0x00000009, // End of structure block
 };
-
 
 /// Header preceding each FDT_PROP token's data in the structure block.
 /// All fields are big-endian.
 pub const PropHeader = extern struct {
-    len: u32,      // Length of property data in bytes
-    nameoff: u32,  // Offset into strings block for property name
+    len: u32, // Length of property data in bytes
+    nameoff: u32, // Offset into strings block for property name
 };
 
+/// Native-endian copy of the header fields for convenient use.
+pub const HeaderNative = struct {
+    totalsize: u32,
+    off_dt_struct: u32,
+    off_dt_strings: u32,
+    off_mem_rsvmap: u32,
+    version: u32,
+    last_comp_version: u32,
+    boot_cpuid_phys: u32,
+    size_dt_strings: u32,
+    size_dt_struct: u32,
+};
 
 /// FDT header, located at the base address passed in x0 by QEMU.
 /// All fields are big-endian.
@@ -56,30 +64,15 @@ pub const Header = extern struct {
 
     pub fn native(self: *const Header) HeaderNative {
         return .{
-            .totalsize         = std.mem.bigToNative(u32, self.totalsize),
-            .off_dt_struct     = std.mem.bigToNative(u32, self.off_dt_struct),
-            .off_dt_strings    = std.mem.bigToNative(u32, self.off_dt_strings),
-            .off_mem_rsvmap    = std.mem.bigToNative(u32, self.off_mem_rsvmap),
-            .version           = std.mem.bigToNative(u32, self.version),
+            .totalsize = std.mem.bigToNative(u32, self.totalsize),
+            .off_dt_struct = std.mem.bigToNative(u32, self.off_dt_struct),
+            .off_dt_strings = std.mem.bigToNative(u32, self.off_dt_strings),
+            .off_mem_rsvmap = std.mem.bigToNative(u32, self.off_mem_rsvmap),
+            .version = std.mem.bigToNative(u32, self.version),
             .last_comp_version = std.mem.bigToNative(u32, self.last_comp_version),
-            .boot_cpuid_phys   = std.mem.bigToNative(u32, self.boot_cpuid_phys),
-            .size_dt_strings   = std.mem.bigToNative(u32, self.size_dt_strings),
-            .size_dt_struct    = std.mem.bigToNative(u32, self.size_dt_struct),
+            .boot_cpuid_phys = std.mem.bigToNative(u32, self.boot_cpuid_phys),
+            .size_dt_strings = std.mem.bigToNative(u32, self.size_dt_strings),
+            .size_dt_struct = std.mem.bigToNative(u32, self.size_dt_struct),
         };
     }
 };
-
-/// Native-endian copy of the header fields for convenient use.
-pub const HeaderNative = struct {
-    totalsize: u32,
-    off_dt_struct: u32,
-    off_dt_strings: u32,
-    off_mem_rsvmap: u32,
-    version: u32,
-    last_comp_version: u32,
-    boot_cpuid_phys: u32,
-    size_dt_strings: u32,
-    size_dt_struct: u32,
-};
-
-
