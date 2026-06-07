4af6f4394288097609f00394f923e4bc73d17e18
Artie Poole <stuart.poole@canonical.com>
2026-06-07 21:25:18 +0100

    recursion? do not like but the indent is nice
    Signed-off-by: Artie Poole <stuart.poole@canonical.com>

--- src/arch/arm64/init/init_dtb.zig
+++ src/arch/arm64/init/init_dtb.zig
@@ -9,7 +9,15 @@ const dtb = @import("artlib").dtb;
 const BeginItem = std.meta.TagPayload(dtb.Item, .begin);
 const PropertyItem = std.meta.TagPayload(dtb.Item, .property);
 
-fn print_node(n: BeginItem) void {
+fn write_indent(depth: usize) void {
+    var i: usize = 0;
+    while (i < depth) : (i += 1) {
+        serial.early_write("  ") catch {};
+    }
+}
+
+fn print_node(n: BeginItem, depth: usize) void {
+    write_indent(depth);
     // print node label
     serial.early_write("Begin node: ") catch {};
 
@@ -23,7 +31,8 @@ fn print_node(n: BeginItem) void {
 }
 
 // Helper: print "node: {node_name}, property: {prop_name}\n"
-fn print_prop(p: PropertyItem) void {
+fn print_prop(p: PropertyItem, depth: usize) void {
+    write_indent(depth);
     // print node label
     serial.early_write("Property: ") catch {};
 
@@ -52,6 +61,40 @@ fn device_type(id: []const u8) device_ids {
     return .other;
 }
 
+fn consume_node(w: *dtb.Walker, n: BeginItem) dtb.Error!void {
+    const node_depth = n.resulting_depth - 1;
+    print_node(n, node_depth);
+
+    const at = std.mem.indexOfScalar(u8, n.name, '@');
+    const id = if (at) |i| n.name[0..i] else n.name;
+    const addr = if (at) |i| n.name[i + 1 ..] else "";
+
+    switch (device_type(id)) {
+        .pl011 => {
+            const addr_usize = std.fmt.parseInt(usize, addr, 16) catch {
+                return dtb.Error.BadHex;
+            };
+            const prime = serial.PrimeCell.init(addr_usize);
+            _ = prime;
+        },
+        .pl031 => {},
+        .pcie => {},
+        .memory => {},
+        .other => {},
+    }
+
+    while (true) {
+        const item = try w.next();
+        switch (item) {
+            .begin => |child| try consume_node(w, child),
+            .property => |p| print_prop(p, n.resulting_depth),
+            .nop => {},
+            .end_node => return,
+            .end => return dtb.Error.UnexpectedToken,
+        }
+    }
+}
+
 pub fn initialise_device_tree(dtb_addr: usize) dtb.Error!void {
     const d = dtb.Dtb.init(dtb_addr) catch |err| {
         // print diagnostic and halt (noreturn)
@@ -75,47 +118,14 @@ pub fn initialise_device_tree(dtb_addr: usize) dtb.Error!void {
         return err;
     };
     var w = d.walker();
-    while (w.next() catch null) |item| {
+    while (true) {
+        const item = try w.next();
         switch (item) {
             .begin => |n| {
-                print_node(n);
-                const at = std.mem.indexOfScalar(u8, n.name, '@');
-                const id = if (at) |i| n.name[0..i] else n.name;
-                const addr = if (at) |i| n.name[i + 1 ..] else "";
-                switch (device_type(id)) {
-                    .pl011 => {
-                        const addr_usize = std.fmt.parseInt(usize, addr, 16) catch {
-                            return dtb.Error.BadHex;
-                        };
-                        const prime = serial.PrimeCell.init(addr_usize);
-                        _ = prime;
-                        while (w.next() catch null) |sub_item| {
-                            switch (sub_item) {
-                                .begin => {
-                                    return error.InvalidNodeToken;
-                                },
-                                .property => |p| {
-                                    print_prop(p);
-                                },
-                                .nop => {},
-                                .end_node => {
-                                    serial.early_write("End Node\n") catch {};
-                                    break;
-                                },
-                                .end => {
-                                    return error.InvalidNodeToken;
-                                },
-                            }
-                        }
-                    },
-                    .pl031 => {},
-                    .pcie => {},
-                    .memory => {},
-                    .other => {},
-                }
+                try consume_node(&w, n);
             },
             .property => |p| {
-                print_prop(p);
+                print_prop(p, 0);
             },
             .nop => {},
             .end_node => {},
